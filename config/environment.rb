3282debfe4298b73ed8f379ea4836900b87a4f7f
SynthesisOne <developerfromrussia@gmail.com>
2020-06-07 15:41:04 +0300

    middleware

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
-require_relative '../lib/simpler'
+# frozen_string_literal: true
 
+require_relative '../lib/simpler'
+require_relative '../lib/simpler/middleware/log'
 Simpler.application.bootstrap!
